0e9adb84f9b584306fde319e652d3f9639b07c27
Jelle <>
2018-01-11 12:07:56 +0100

    Priority queue translation finished

--- priority-queue.lua
+++ priority-queue.lua
@@ -1,6 +1,31 @@
+-- Set up
 a = {3, 20, 11, 15, 33, 2, 50}
 n = 7
 
+-- Priority queue class
+PriorityQueue = {}
+PriorityQueue.__index = PriorityQueue
+function PriorityQueue:create(array)
+  local prioq = {}
+  setmetatable(prioq, PriorityQueue)
+  make_heap(array)
+  prioq.array = array
+  return prioq
+end
+
+function PriorityQueue:push(k)
+  push_heap(self.array, k)
+end
+
+function PriorityQueue:pop()
+  return pop_heap(self.array)
+end
+
+function PriorityQueue:top()
+  return peak_heap(self.array)
+end
+
+-- Helper functions
 function divide(a,b)
     return (a - a % b) / b
 end
@@ -24,7 +49,7 @@ function right(i)
 end
 
 function sift_up(array, i)
-    if(i == 1 or array[i] >= A[parent(i)]) then
+    if(i == 1 or array[i] <= array[parent(i)]) then
         return
     end
     swap(array, i, parent(i))
@@ -33,10 +58,10 @@ end
 
 function sift_down(array, i)
     j = i
-    if left(i) <= n and array[left(i)] < array[j] then
+    if left(i) <= n and array[left(i)] > array[j] then
         j = left(i)
     end
-    if right(i) <= n and array[right(i)] < array[j] then
+    if right(i) <= n and array[right(i)] > array[j] then
         j = right(i)
     end
     if j ~= i then
@@ -45,6 +70,7 @@ function sift_down(array, i)
     end
 end    
 
+-- Heap functions
 function make_heap(array)
     p = divide(n, 2)
     while p ~= 0 do
@@ -62,6 +88,7 @@ end
 function pop_heap(array)
     element = array[1]
     array[1] = array[n]
+    array[n] = nil
     n = n - 1
     sift_down(array, 1)
     return element
@@ -71,6 +98,11 @@ function peak_heap(array)
     return array[1]
 end
     
-make_heap(a)
-print(a[1])
-
+-- Testing     
+test = PriorityQueue:create(a)
+print(test:top())
+print(test:pop())
+test:push(10000)
+print(test:top())
+print(test:pop())
+print("test")
